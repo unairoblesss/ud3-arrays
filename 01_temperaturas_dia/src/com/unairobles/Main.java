529b50f7269ad3447fb7d7f9da074ff14a8b11d7
Unknown <unai.robles@ikasle.egibide.org>
2017-11-22 21:03:56 +0100

    01_TEMPERATURAS_DIA_01
    -El mensaje que devuelve los resultados no es como el propuesto en el ejercicio

--- 01_temperaturas_dia/src/com/unairobles/Main.java
+++ 01_temperaturas_dia/src/com/unairobles/Main.java
@@ -11,6 +11,8 @@ public class Main {
 
         double min = 1000;
         double max = 0;
+        double n = 0;
+        double m = 0;
 
         double[] temperaturas;
         temperaturas = new double[24];
@@ -18,15 +20,19 @@ public class Main {
         for (int i = 0; i < temperaturas.length; i++) {
             System.out.print("Escribela temperatura de cada hora: ");
             temperaturas[i] = Double.parseDouble(br.readLine());
-
-            if (temperaturas[i] < min) {
-                min = temperaturas[i];
-            } else if (temperaturas[i] > max) {
-                max = temperaturas[i];
-            }
+            do {
+                if (temperaturas[i] < min) {
+                    min = temperaturas[i];
+                } else if (temperaturas[i] > max) {
+                    max = temperaturas[i];
+                }
+                n = n + temperaturas[i];
+            } while (temperaturas[i] < 100 && temperaturas[i] > 0);
         }
+        m = n / 24;
         System.out.println(max);
         System.out.println(min);
+        System.out.println(m);
 
     }
 }
